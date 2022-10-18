013233c68c97a96466564449ebcce46f7239e7ec
Melissa Wu <melissa.o.wu@outlook.com>
2022-10-17 20:00:14 -0400

    Updates

---
+++ precache-manifest.52c9f6df45ec18fed9b5ffdb26b5cebb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "cc7b5d902fb4c6ecd765",
+    "url": "/personal-website/static/css/main.ac657323.chunk.css"
+  },
+  {
+    "revision": "cc7b5d902fb4c6ecd765",
+    "url": "/personal-website/static/js/main.cc7b5d90.chunk.js"
+  },
+  {
+    "revision": "4e3eec0efd1bea118b1d",
+    "url": "/personal-website/static/js/1.4e3eec0e.chunk.js"
+  },
+  {
+    "revision": "7d7bfacee97532433a8d",
+    "url": "/personal-website/static/js/2.7d7bface.chunk.js"
+  },
+  {
+    "revision": "75b93d4355a13971434a",
+    "url": "/personal-website/static/js/runtime~main.75b93d43.js"
+  },
+  {
+    "revision": "5e2769207f1b2737721f61b1cc249c6c",
+    "url": "/personal-website/static/media/avataaars.5e276920.png"
+  },
+  {
+    "revision": "8e5c9be2c3d286c185f407d86a49d828",
+    "url": "/personal-website/static/media/stackOverflow.8e5c9be2.png"
+  },
+  {
+    "revision": "4c1038b603780b0fbdc2c06edaa2e33d",
+    "url": "/personal-website/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/personal-website/precache-manifest.2623ded94999af186cae441a2ecbe5d3.js"
+  "/personal-website/precache-manifest.52c9f6df45ec18fed9b5ffdb26b5cebb.js"
 );
 
 workbox.clientsClaim();
